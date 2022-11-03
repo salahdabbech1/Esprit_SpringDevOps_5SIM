6817e3b854d9c49c7539df37425eb4af0e3460ae
salahdabbech1 <salah.dabbech@esprit.tn>
2022-11-03 21:29:10 +0100

    fix : changed nexus url in pom

--- src/main/java/com/esprit/examen/controllers/SecteurActiviteController.java
+++ src/main/java/com/esprit/examen/controllers/SecteurActiviteController.java
@@ -36,8 +36,7 @@ public class SecteurActiviteController {
 	@PostMapping("/add-secteurActivite")
 	@ResponseBody
 	public SecteurActivite addSecteurActivite(@RequestBody SecteurActivite sa) {
-		SecteurActivite secteurActivite = secteurActiviteService.addSecteurActivite(sa);
-		return secteurActivite;
+		return secteurActiviteService.addSecteurActivite(sa);
 	}
 
 	// http://localhost:8089/SpringMVC/secteurActivite/remove-secteurActivite/{secteurActivite-id}
