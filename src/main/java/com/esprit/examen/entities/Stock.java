6657fe83ceb8dafd424fc300da41708d1d489c46
salahdabbech1 <salah.dabbech@esprit.tn>
2022-11-03 22:28:05 +0100

    fix: changes stock test and added mockito

--- src/main/java/com/esprit/examen/entities/Stock.java
+++ src/main/java/com/esprit/examen/entities/Stock.java
@@ -1,10 +1,7 @@
 package com.esprit.examen.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -12,6 +9,7 @@ import java.util.Set;
 
 @Entity
 @Getter
+@Builder
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
