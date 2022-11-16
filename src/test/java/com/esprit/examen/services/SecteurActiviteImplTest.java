6c8ff02246d8e7ad8d9c3bb1552faff2276402fa
salahdabbech1 <salah.dabbech@esprit.tn>
2022-11-17 00:04:22 +0100

    fix:reverted changes

--- src/test/java/com/esprit/examen/services/SecteurActiviteImplTest.java
+++ src/test/java/com/esprit/examen/services/SecteurActiviteImplTest.java
@@ -2,107 +2,37 @@ package com.esprit.examen.services;
 
 import com.esprit.examen.entities.SecteurActivite;
 import com.esprit.examen.repositories.SecteurActiviteRepository;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-class SecteurServiceImplTest {
-
+class SecteurActiviteImplTest {
 
     @Mock
-    SecteurActiviteRepository sr;
+    SecteurActiviteRepository secteurActiviteRepository;
+
     @InjectMocks
-    SecteurActiviteServiceImpl ss;
+    SecteurActiviteServiceImpl secteurActiviteService;
 
+    SecteurActivite secteurActivite = new SecteurActivite("Test code secteur", "testlibelleSecteur");
 
-    SecteurActivite s = SecteurActivite.builder().codeSecteurActivite("111ee").libelleSecteurActivite("secteur test").build();
-    List<SecteurActivite> listSecteur = new ArrayList<SecteurActivite>() {
-
-        private static final long serialVersionUID = 1L;
-
+    List<SecteurActivite> listSecteurActive = new ArrayList<SecteurActivite>() {
         {
-            add(SecteurActivite.builder().codeSecteurActivite("22b").libelleSecteurActivite("secteur test").build());
-            add(SecteurActivite.builder().codeSecteurActivite("333c").libelleSecteurActivite("secteur test").build());
-
+            add(SecteurActivite.builder().codeSecteurActivite("Test code secteur2").libelleSecteurActivite("testlibelleSecteur2").build());
+            add(SecteurActivite.builder().codeSecteurActivite("Test code secteur3").libelleSecteurActivite("testlibelleSecteur3").build());
         }
     };
 
-
     @Test
-    public void testAddSecteur() {
-        Mockito.when(sr.save(s)).thenReturn(s);
-        SecteurActivite secteuradd = ss.addSecteurActivite(s);
-        Assertions.assertNotNull(secteuradd);
+    void testRetriveAllSecteurActivite() {
+        secteurActiviteRepository.findAll();
     }
-
-    @Test
-    public void testUpdateSecteur() {
-        s.setLibelleSecteurActivite("secteur aicha");
-        Mockito.when(sr.save(s)).thenReturn(s);
-        SecteurActivite sa = ss.updateSecteurActivite(s);
-        Assertions.assertEquals(s, sa);
-    }
-
-    @Test
-    public void RetrieveAllTest() {
-        List<SecteurActivite> s = new ArrayList<>();
-        s.add(new SecteurActivite());
-
-        when(sr.findAll()).thenReturn(s);
-
-        List<SecteurActivite> expected = ss.retrieveAllSecteurActivite();
-
-        //log.info("get ==> " + String.valueOf(s));
-
-        assertEquals(expected, s);
-        verify(sr).findAll();
-    }
-
-
-    @Test
-    public void DeleteSecteurTest() throws Exception {
-        SecteurActivite s = new SecteurActivite();
-        s.setCodeSecteurActivite("22buu");
-        s.setLibelleSecteurActivite("del aicha");
-
-        Mockito.when(sr.findById(s.getIdSecteurActivite())).thenReturn(Optional.of(s));
-        ss.deleteSecteurActivite(s.getIdSecteurActivite());
-        verify(sr).deleteById(s.getIdSecteurActivite());
-
-    }
-
-
-    @Test()
-    public void should_throw_exception_when_user_doesnt_exist() {
-        SecteurActivite s = new SecteurActivite();
-        s.setCodeSecteurActivite("111111");
-        s.setLibelleSecteurActivite("secteur nouv");
-
-        given(sr.findById(anyLong())).willReturn(Optional.ofNullable(null));
-        ss.deleteSecteurActivite(s.getIdSecteurActivite());
-
-
-    }
-
-
 }
