dee91e743486dbb68365e6f747180fcdc1b1b8b9
salahdabbech1 <salah.dabbech@esprit.tn>
2022-11-17 11:02:30 +0100

    feat:finished Sa tests

--- src/test/java/com/esprit/examen/services/SecteurActiviteImplTest.java
+++ src/test/java/com/esprit/examen/services/SecteurActiviteImplTest.java
@@ -1,16 +1,20 @@
 package com.esprit.examen.services;
 
 import com.esprit.examen.entities.SecteurActivite;
+import com.esprit.examen.entities.Stock;
 import com.esprit.examen.repositories.SecteurActiviteRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -33,6 +37,34 @@ class SecteurActiviteImplTest {
 
     @Test
     void testRetriveAllSecteurActivite() {
-        secteurActiviteRepository.findAll();
+        Mockito.when(secteurActiviteRepository.findAll()).thenReturn(listSecteurActive);
+        List<SecteurActivite> lSA=secteurActiviteService.retrieveAllSecteurActivite();
+        Assertions.assertNotNull(lSA);
+
+    }
+    @Test
+    void testAddSecteurActivite(){
+        Mockito.when(secteurActiviteRepository.save(secteurActivite)).thenReturn(secteurActivite);
+        SecteurActivite SA = secteurActiviteService.addSecteurActivite(secteurActivite);
+        Assertions.assertEquals(secteurActivite, SA);
+    }
+    @Test
+    void testDeletSecteurActivite(){
+        secteurActiviteService.deleteSecteurActivite(secteurActivite.getIdSecteurActivite());
+        Mockito.verify(secteurActiviteRepository, Mockito.times(1)).deleteById(secteurActivite.getIdSecteurActivite());
+    }
+    @Test
+    void testRetriveOneSecteurActivite(){
+        Mockito.when(secteurActiviteRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(secteurActivite));
+        @SuppressWarnings("removal")
+        SecteurActivite SA = secteurActiviteService.retrieveSecteurActivite(new Long(2));
+        Assertions.assertNotNull(SA);
+    }
+    @Test
+    void testUpdateSecteurActivite(){
+        secteurActivite.setCodeSecteurActivite("8");
+        Mockito.when(secteurActiviteRepository.save(secteurActivite)).thenReturn(secteurActivite);
+        SecteurActivite SA = secteurActiviteService.updateSecteurActivite(secteurActivite);
+        Assertions.assertEquals(secteurActivite, SA);
     }
 }
